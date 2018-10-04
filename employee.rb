7cc7098315929e26cd59cb8e955c2a4e448acf68
Peter Jang <peter.x.jang@gmail.com>
2018-10-03 19:55:34 -0500

    Use hash with symbols to model employee

--- employee.rb
+++ employee.rb
@@ -9,3 +9,9 @@ employee1 = {"first_name" => "Majora", "last_name" => "Carter", "salary" => 8000
 employee2 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
 puts "#{employee1["first_name"]} #{employee1["last_name"]} makes #{employee1["salary"]} a year."
 puts "#{employee2["first_name"]} #{employee2["last_name"]} makes #{employee2["salary"]} a year."
+
+# Employee using hash with symbols
+employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true}
+employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: true}
+puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
+puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
