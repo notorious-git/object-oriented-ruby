728124752e11f8f4858be1b3dee0d34515e68751
Peter Jang <peter.x.jang@gmail.com>
2018-10-04 18:51:47 -0500

    Use options hash instead of multiple arguments

--- employee.rb
+++ employee.rb
@@ -21,11 +21,11 @@ class Employee
   attr_reader :first_name, :last_name, :active
   attr_writer :active
 
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+  def initialize(input_options_hash)
+    @first_name = input_options_hash[:first_name]
+    @last_name = input_options_hash[:last_name]
+    @salary = input_options_hash[:salary]
+    @active = input_options_hash[:active]
   end
 
   def print_info
@@ -37,8 +37,8 @@ class Employee
   end
 end
 
-employee1 = Employee.new("Majora", "Carter", 80000, true)
-employee2 = Employee.new("Danilo", "Campos", 70000, true)
+employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
+employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
 employee1.print_info
 employee2.print_info
 employee1.give_annual_raise
