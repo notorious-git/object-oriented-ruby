2958d747696439213f4d61adf5bb8d9a70e67a2d
Peter Jang <peter.x.jang@gmail.com>
2018-10-03 19:41:26 -0500

    Use array to model employee

---
+++ employee.rb
@@ -0,0 +1,5 @@
+# Employee using array
+employee1 = ["Majora", "Carter", 80000, true]
+employee2 = ["Danilo", "Campos", 70000, true]
+puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
+puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
