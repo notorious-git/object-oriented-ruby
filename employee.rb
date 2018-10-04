0ad201c18183eeb2f0539f5f13bee221245690ff
Peter Jang <peter.x.jang@gmail.com>
2018-10-03 20:57:36 -0500

    Use class object to model employee

--- employee.rb
+++ employee.rb
@@ -15,3 +15,51 @@ employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, 
 employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: true}
 puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
 puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
+
+# Employee using class object
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def active
+    @active
+  end
+
+  def active=(input_new_active)
+    @active = input_new_active
+  end
+
+  def print_info
+    puts "#{first_name} #{last_name} makes #{@salary} a year."
+  end
+
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+end
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
+employee2 = Employee.new("Danilo", "Campos", 70000, true)
+employee1.print_info
+employee2.print_info
+employee1.give_annual_raise
+employee1.print_info
+
+puts employee1.first_name
+puts employee1.last_name
+
+puts employee2.active
+employee2.active = false
+puts employee2.active
