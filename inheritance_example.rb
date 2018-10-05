6f23b96bbb9e71148512fccaae026d0496d6cd6b
Peter Jang <peter.x.jang@gmail.com>
2018-10-04 20:36:33 -0500

    Use super to customize car/bike initialize methods

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -18,19 +18,34 @@ class Vehicle
 end
 
 class Car < Vehicle
+  def initialize(input_fuel, input_make, input_model)
+    super()
+    @fuel = input_fuel
+    @make = input_make
+    @model = input_model
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  def initialize(input_gears, input_type, input_weight, input_color)
+    super()
+    @gears = input_gears
+    @type = input_type
+    @weight = input_weight
+    @color = input_color
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-my_car = Car.new
+my_car = Car.new(5, "honda", "civic")
 my_car.honk_horn
 
-my_bike = Bike.new
+my_bike = Bike.new(10, "mountain", 20, "blue")
 my_bike.ring_bell
