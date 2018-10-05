029a2eae4acb5e1366e8ad09c73ce53482b76b2f
Peter Jang <peter.x.jang@gmail.com>
2018-10-04 19:17:53 -0500

    Add manager example with inheritance

---
+++ manager.rb
@@ -0,0 +1,31 @@
+class Employee
+  attr_reader :first_name, :last_name, :active
+  attr_writer :active
+
+  def initialize(input_options_hash)
+    @first_name = input_options_hash[:first_name]
+    @last_name = input_options_hash[:last_name]
+    @salary = input_options_hash[:salary]
+    @active = input_options_hash[:active]
+  end
+
+  def print_info
+    puts "#{first_name} #{last_name} makes #{@salary} a year."
+  end
+
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+end
+
+class Manager < Employee
+  def send_report
+    puts "Sending email..."
+    # use email sending library
+    puts "Email sent!"
+  end
+end
+
+manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true)
+manager.print_info
+manager.send_report
