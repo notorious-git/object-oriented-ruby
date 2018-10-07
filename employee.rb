6eb1544354862b7a425db28821dca91c3f7280ca
Peter Jang <peter.x.jang@gmail.com>
2018-10-07 10:17:53 -0500

    Refactor each class and module into separate files

--- employee.rb
+++ employee.rb
@@ -1,22 +1,3 @@
-# Employee using array
-employee1 = ["Majora", "Carter", 80000, true]
-employee2 = ["Danilo", "Campos", 70000, true]
-puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
-puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
-
-# Employee using hash
-employee1 = {"first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true}
-employee2 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
-puts "#{employee1["first_name"]} #{employee1["last_name"]} makes #{employee1["salary"]} a year."
-puts "#{employee2["first_name"]} #{employee2["last_name"]} makes #{employee2["salary"]} a year."
-
-# Employee using hash with symbols
-employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true}
-employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: true}
-puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
-puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
-
-# Employee using class object
 class Employee
   attr_reader :first_name, :last_name, :active
   attr_writer :active
@@ -36,17 +17,3 @@ class Employee
     @salary = 1.05 * @salary
   end
 end
-
-employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
-employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-employee1.print_info
-employee2.print_info
-employee1.give_annual_raise
-employee1.print_info
-
-puts employee1.first_name
-puts employee1.last_name
-
-puts employee2.active
-employee2.active = false
-puts employee2.active
--- manager.rb
+++ manager.rb
@@ -1,33 +1,12 @@
-class Employee
-  attr_reader :first_name, :last_name, :active
-  attr_writer :active
-
-  def initialize(input_options_hash)
-    @first_name = input_options_hash[:first_name]
-    @last_name = input_options_hash[:last_name]
-    @salary = input_options_hash[:salary]
-    @active = input_options_hash[:active]
-  end
-
-  def print_info
-    puts "#{first_name} #{last_name} makes #{@salary} a year."
-  end
-
-  def give_annual_raise
-    @salary = 1.05 * @salary
-  end
-end
+require "./employee.rb"
+require "./email_reportable.rb"
 
 class Manager < Employee
+  include EmailReportable
+
   def initialize(input_options_hash)
     super
     @employees = input_options_hash[:employees]
-  end
-
-  def send_report
-    puts "Sending email..."
-    # use email sending library
-    puts "Email sent!"
   end
 
   def give_all_raises
@@ -49,21 +28,3 @@ class Manager < Employee
     end
   end
 end
-
-employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
-employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-
-
-manager = Manager.new(
-  first_name: "Saron",
-  last_name: "Yitbarek",
-  salary: 100000,
-  active: true,
-  employees: [employee1, employee2]
-)
-manager.print_info
-manager.send_report
-p manager
-manager.give_all_raises
-manager.fire_all_employees
-p manager
